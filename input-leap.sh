e93bbd75f5d85da9a15204f34199701a93253e49
Thiago Negri <thiago.negri@babycenter.com>
2025-10-07 07:41:40 -0300

    mac input-leap

--- input-leap.sh
+++ input-leap.sh
@@ -3,3 +3,5 @@
 brew reinstall input-leap
 tccutil reset Accessibility input-leap
 open "$(brew --prefix)/opt/input-leap/InputLeap.app"
+read -p 'ready to open it again? (Y/n) ' ready
+[ "$ready" -ne "n" ] && open "$(brew --prefix)/opt/input-leap/InputLeap.app"
