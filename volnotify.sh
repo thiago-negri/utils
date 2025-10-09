2a0a3107e211a6ac185ee703d294d3b6329400db
thiago-negri <evohunz@gmail.com>
2025-10-09 09:11:11 -0300

    fix emiting empty vol

--- volnotify.sh
+++ volnotify.sh
@@ -15,14 +15,22 @@ vol() {
     fi
 }
 
-# The pipes spawns 3 sub processes (pactl, grep, while loop).
-# Killing the while loop (using $! pid) will not kill the grep, nor the pactl.
+emit() {
+    local v
+    v=$(vol)
+    [ -z "$v" ] || printf "vol\t$v\n"
+}
+
+# The pipe spawn sub processes (pactl + while loop).
+# Killing the while loop (using $! pid) will not kill the pactl.
 # Easiest way I found to avoid leaking processes if I reload my panel is to 'wait' and kill jobs on SIGINT|SIGTERM
 trap 'kill $(jobs -p)' INT TERM
 
-printf "vol\t$(vol)\n"
-pactl subscribe | grep --line-buffered 'change' | while read -r _; do
-    printf "vol\t$(vol)\n"
+emit
+pactl subscribe | while read -r line; do
+    if [[ "$line" =~ 'change' ]]; then
+        emit
+    fi
 done &
 
 wait
