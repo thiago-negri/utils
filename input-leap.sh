1ed34c8fd4b7221ac73d8e7bbc159e7e103dfbb6
Thiago Negri <thiago.negri@babycenter.com>
2025-10-14 18:22:25 -0300

    loop input leap

--- input-leap.sh
+++ input-leap.sh
@@ -1,7 +1,10 @@
-#!/bin/sh
+#!/usr/bin/env bash
 [ "$(uname)" = "Darwin" ] || exit 1
 brew reinstall input-leap
 tccutil reset Accessibility input-leap
 open "$(brew --prefix)/opt/input-leap/InputLeap.app"
-read -p 'ready to open it again? (Y/n) ' ready
-[ ! "$ready" = "n" ] && open "$(brew --prefix)/opt/input-leap/InputLeap.app"
+read -p 'open it again? (Y/n) ' ready
+while [ "$ready" != "n" ]; do
+  open "$(brew --prefix)/opt/input-leap/InputLeap.app"
+  read -p 'open it again? (Y/n) ' ready
+done
