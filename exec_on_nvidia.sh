5ae59368dae08ffaa2cbb15b831174e0604715e7
thiago-negri <evohunz@gmail.com>
2025-12-30 20:27:44 -0300

    add exec_on_nvidia.sh

---
+++ exec_on_nvidia.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+export __NV_PRIME_RENDER_OFFLOAD=1
+export __GLX_VENDOR_LIBRARY_NAME=nvidia
+
+exec "$@"
--- setup_monitors.sh
+++ setup_monitors.sh
@@ -4,3 +4,6 @@ xrandr \
   --output eDP-1 --mode 1680x1050 --pos 0x0 \
   --output DP-2 --mode 2560x1440 --pos 1680x160 --right-of eDP-1 --primary
 
+# xrandr \
+#   --output eDP-1-1 --mode 1680x1050 --pos 0x0 \
+#   --output DP-1-2 --mode 2560x1440 --pos 1680x160 --right-of eDP-1-1 --primary
